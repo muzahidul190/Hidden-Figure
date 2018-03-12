5de15a988fbfa21d5fc53e0ebdba29f49859795e
muzahidul190 <muzahidul190@gmail.com>
2018-03-12 23:41:07 +0600

    Update me.js

--- me.js
+++ me.js
@@ -6,7 +6,7 @@ me.on("ready", async () => {
   me.user.setActivity('Coding Discord Bot on github', { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'Coding'}`))
   .catch(console.error);
-  me.user.setStatus('dnd');
+  me.user.setStatus('idle');
 });
 
 me.login(process.env.BOT_TOKEN);
