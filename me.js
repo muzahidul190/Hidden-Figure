c3f1b24cef9e9dc9a573ac4a4129372fa8dcf100
muzahidul190 <muzahidul190@gmail.com>
2018-03-13 09:14:30 +0600

    Update me.js

--- me.js
+++ me.js
@@ -3,13 +3,13 @@ const me = new Discord.Client({disableEveryone: true});
 
 me.on("ready", async () => {
   console.log(`${me.user.username} is online`);
-  me.user.setActivity('Coding Discord Bot!!!', { type: 'WATCHING' })
+  me.user.setActivity('Text Books📱!', { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'Coding'}`))
   .catch(console.error);
   me.user.setStatus('dnd');
 });
 me.on("message", async msg => {
-  if(msg === `HF`) return message.channel.send(`Here I'm....`);
+  if(msg == `HF`) return message.channel.send(`Here I'm....`);
 });
 
 me.login(process.env.BOT_TOKEN);
